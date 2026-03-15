1d9a4e17192cb576d9be2f7fe32e1324a7cc7e87
Alexander Fiedler <alexf137@posteo.de>
2026-03-15 20:10:07 +0100

    Message input component added

--- src/features/chat/components/MessageInput.tsx
+++ src/features/chat/components/MessageInput.tsx
@@ -1,7 +1,8 @@
 function MessageInput() {
     return (
-        <div className="flex flex-row w-full justify-center relative top-[90vh]">
-            <input type="text" className="bg-white w-1/4"/>
+        <div className="flex flex-row w-full justify-center relative top-[90vh] gap-3">
+            <input type="text" className="bg-white rounded-md w-3/4 accent-black p-2"/>
+            <button type="button" className="outline-solid hover:bg-black hover:text-white cursor-pointer border-black rounded-md p-1">Send</button>
         </div>
     );
 }
