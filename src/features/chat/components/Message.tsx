6c7dce009977cf4d095326d410dd9272194eb7d9
Alexander Fiedler <alexf137@posteo.de>
2026-03-22 14:19:22 +0100

    User messages and llm messages are now justified differently

--- src/features/chat/components/Message.tsx
+++ src/features/chat/components/Message.tsx
@@ -1,9 +1,10 @@
 function Message({content, type}: {content: string, type: string}) {
-    const userTypeStyles = "bg-gray-700 p-4 border rounded-md";
-    const userStyleTextColor = "text-white";
+    const userTypeStyles = "justify-end";
+    const llmTypeStyles = "";
+    const userStyleTextColor = "text-white bg-gray-700 p-4 border rounded-md";
 
     return (
-        <div className={type === "user" ? userTypeStyles : ""}>
+        <div className={`${type === "user" ? userTypeStyles : llmTypeStyles} flex w-full`}>
             <p className={type === "user" ? userStyleTextColor : ""}>{content}</p>
         </div>
     );
