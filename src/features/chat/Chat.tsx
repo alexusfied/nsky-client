3bde53884302c9f10ef14f9ce0dc13db689dd203
Alexander Fiedler <alexf137@posteo.de>
2026-03-26 23:22:10 +0100

    Fixed overflow behavior for chat container

--- src/features/chat/Chat.tsx
+++ src/features/chat/Chat.tsx
@@ -10,8 +10,8 @@ function Chat() {
     const {sendMessage, isStreaming, error} = useSendMessage();
 
     return (
-        <section className="flex justify-center h-screen w-full bg-primary-variant pl-60 pr-60 pt-4">
-            <div className="flex flex-col gap-4 h-screen w-full overflow-scroll">
+        <section className="flex justify-center h-screen w-full bg-primary-variant pl-60 pr-60 pt-4 pb-40 overflow-scroll">
+            <div className="flex flex-col gap-4 h-screen w-full">
                 {
                     messages.map((message, id) => <Message content={message.content} type={message.role} key={id}/>)
                 }
