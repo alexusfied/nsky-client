366ce938cfdc318b25b59da67bb67310eca03b03
Alexander Fiedler <alexf137@posteo.de>
2026-04-10 21:38:04 +0200

    Blur + darkening effect added when popup dialog is active

--- src/shared/components/PopupDialog.tsx
+++ src/shared/components/PopupDialog.tsx
@@ -9,13 +9,16 @@ interface IPopupDialogProps {
 
 function PopupDialog({onConfirm, onCancel, ref, text}: IPopupDialogProps) {
     return (
-        <div className={"fixed top-1/2 left-1/2 p-4 bg-primary rounded-md flex flex-col gap-4"} ref={ref}>
-            <p className={"text-white"}>{text}</p>
-            <div className={"flex gap-3"}>
-                <button onClick={onCancel} className={"bg-gray-500 p-2 rounded-md cursor-pointer hover:bg-gray-700"}>Cancel</button>
-                <button onClick={onConfirm} className={"bg-red-400 p-2 rounded-md cursor-pointer hover:bg-red-700"}>Confirm</button>
+        <>
+            <div className={"w-screen h-screen z-30 fixed top-0 left-0 backdrop-blur-sm opacity-50 bg-black"}></div>
+            <div className={"fixed top-1/2 left-1/2 p-4 bg-primary rounded-md flex flex-col gap-4 z-40"} ref={ref}>
+                <p className={"text-white"}>{text}</p>
+                <div className={"flex gap-3"}>
+                    <button onClick={onCancel} className={"bg-gray-500 p-2 rounded-md cursor-pointer hover:bg-gray-700"}>Cancel</button>
+                    <button onClick={onConfirm} className={"bg-red-400 p-2 rounded-md cursor-pointer hover:bg-red-700"}>Confirm</button>
+                </div>
             </div>
-        </div>
+        </>
     );
 }
 
