f9745e823058fd28f869f713f18235022065ea94
Alex <alexf137@posteo.de>
2026-07-26 22:15:58 +0200

    Autoscroll now stops when a user scrolls up during streaming

--- src/features/chat/hooks/useAutoScroll.ts
+++ src/features/chat/hooks/useAutoScroll.ts
@@ -2,15 +2,20 @@ import {useEffect, useRef, useState} from "react";
 import useMessageStore from "@/shared/store/messageStore.ts";
 
 function useAutoScroll() {
-    const [autoScroll, setAutoScroll] = useState(true);
     const chatRef = useRef<HTMLDivElement>(null);
     const messages = useMessageStore((store) => store.messages);
 
     useEffect(() => {
         const chatRefCurrent = chatRef.current;
 
-        if (chatRefCurrent && autoScroll) {
-            chatRefCurrent.scrollTop = chatRefCurrent.scrollHeight;
+        if (!(Math.abs(chatRefCurrent.scrollHeight - chatRefCurrent.clientHeight - chatRefCurrent.scrollTop) <= 300)) {
+            return;
+        }
+        if (chatRefCurrent) {
+            chatRefCurrent.scrollTo({
+                top: chatRefCurrent.scrollHeight,
+                behavior: "smooth"
+            }); 
         }
     }, [messages]);
 
