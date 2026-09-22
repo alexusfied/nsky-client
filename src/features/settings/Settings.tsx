d54606c8f650c1baa8104433cfb717ab6fc2dce7
Alex <alexf137@posteo.de>
2026-09-22 22:10:20 +0200

    Settings dialog moved to its own component

---
+++ src/features/settings/Settings.tsx
@@ -0,0 +1,49 @@
+import useSettingsStore from "@/shared/store/settingsStore.ts";
+import PopupDialog from "@/shared/components/PopupDialog.tsx";
+
+function Settings() {
+    const selectedProvider = useSettingsStore((state) => state.selectedProvider);
+    const setSelectedProvider = useSettingsStore((state) => state.setSelectedProvider);
+    const providerList = useSettingsStore((state) => state.providerList);
+    const themesList = useSettingsStore((state) => state.themesList);
+    const selectedTheme = useSettingsStore((state) => state.selectedTheme);
+    const setSelectedTheme = useSettingsStore((state) => state.setSelectedTheme);
+    const setShowSettingsDialog = useSettingsStore((state) => state.setShowSettingsDialog);
+
+    return (
+        <PopupDialog
+              onConfirm={() => {}}
+              onCancel={() => {
+                  setShowSettingsDialog(false);
+              }}
+              hideButtons={true}
+              children={
+                  <div className={`flex flex-col gap-3`}>
+                      {
+                          // TODO: Selection rows should also be their own component 
+                      }  
+                      <div className={`flex justify-between w-[15vw]`}>
+                          <label htmlFor={`provider`} className={`font-semibold`}>Provider</label>
+                          <select defaultValue={selectedProvider} name={`provider`} id={`provider`} onChange={(event) => {setSelectedProvider(event.target.value)}}>
+                              {
+                                  providerList.map((provider) => <option>{provider}</option>)
+                              }
+                          </select>
+                      </div>
+                      <hr />
+                      <div className={`flex justify-between w-[15vw]`}>
+                          <label htmlFor={`theme`} className={`font-semibold`}>Theme</label>
+                          <select defaultValue={selectedTheme} name={`theme`} id={`theme`} onChange={(event) => {setSelectedTheme(event.target.value)}}>
+                              {
+                                  themesList.map((theme) => <option>{theme}</option>)
+                              }
+                          </select>
+                      </div>
+                  </div>
+              }
+            
+        />
+    );
+}
+
+export default Settings;
--- src/pages/ChatPage.tsx
+++ src/pages/ChatPage.tsx
@@ -1,17 +1,10 @@
 import Conversations from "../features/conversations/Conversations";
 import Chat from "../features/chat/Chat";
-import PopupDialog from "@/shared/components/PopupDialog.tsx";
+import Settings from "../features/settings/Settings.tsx"
 import useSettingsStore from "@/shared/store/settingsStore.ts";
 
 function ChatPage() {
     const showSettingsDialog = useSettingsStore((state) => state.showSettingsDialog);
-    const setShowSettingsDialog = useSettingsStore((state) => state.setShowSettingsDialog);
-    const selectedProvider = useSettingsStore((state) => state.selectedProvider);
-    const setSelectedProvider = useSettingsStore((state) => state.setSelectedProvider);
-    const providerList = useSettingsStore((state) => state.providerList);
-    const themesList = useSettingsStore((state) => state.themesList);
-    const selectedTheme = useSettingsStore((state) => state.selectedTheme);
-    const setSelectedTheme = useSettingsStore((state) => state.setSelectedTheme);
 
     return (
         <div className="flex flex-row h-screen">
@@ -20,38 +13,7 @@ function ChatPage() {
                 // TODO: Move settings dialog to its own component 
             }
             <Chat />
-            { showSettingsDialog && <PopupDialog
-              onConfirm={() => {}}
-              onCancel={() => {
-                  setShowSettingsDialog(false);
-              }}
-              hideButtons={true}
-              children={
-                  <div className={`flex flex-col gap-3`}>
-                      {
-                          // TODO: Selection rows should also be their own component 
-                      }  
-                      <div className={`flex justify-between w-[15vw]`}>
-                          <label htmlFor={`provider`} className={`font-semibold`}>Provider</label>
-                          <select defaultValue={selectedProvider} name={`provider`} id={`provider`} onChange={(event) => {setSelectedProvider(event.target.value)}}>
-                              {
-                                  providerList.map((provider) => <option>{provider}</option>)
-                              }
-                          </select>
-                      </div>
-                      <hr />
-                      <div className={`flex justify-between w-[15vw]`}>
-                          <label htmlFor={`theme`} className={`font-semibold`}>Theme</label>
-                          <select defaultValue={selectedTheme} name={`theme`} id={`theme`} onChange={(event) => {setSelectedTheme(event.target.value)}}>
-                              {
-                                  themesList.map((theme) => <option>{theme}</option>)
-                              }
-                          </select>
-                      </div>
-                  </div>
-              }
-            />
-            }
+            { showSettingsDialog && <Settings /> }
         </div>
     );
 }
